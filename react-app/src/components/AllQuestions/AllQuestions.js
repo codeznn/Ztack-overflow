c8f5ce9dcd20d6876d3940fc06e028df51efeb9f
Nannan Zhang <zhangnn8811@gmail.com>
2023-01-03 14:27:04 -0800

    questions.css

--- react-app/src/components/AllQuestions/AllQuestions.js
+++ react-app/src/components/AllQuestions/AllQuestions.js
@@ -92,7 +92,7 @@ const AllQuestions = () => {
                                     <div className='questions-single-question-title'>
                                         <NavLink to={`/questions/${question.id}`} style={{ textDecoration: 'none'}}>{question.title}</NavLink>
                                     </div>
-                                    {question.body.length > 180?
+                                                                        {question.body.length > 180?
                                         <div className='questions-single-question-body'>{question.body.slice(0,180)}...</div>
                                         :
                                         <div className='questions-single-question-body'>{question.body}</div>
