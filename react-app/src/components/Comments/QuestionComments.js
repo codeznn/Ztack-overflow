46984d0152fc26abffebddf05810e3ac11ffc60c
Nannan Zhang <zhangnn8811@gmail.com>
2023-04-26 20:58:55 -0700

    showbox

--- react-app/src/components/Comments/QuestionComments.js
+++ react-app/src/components/Comments/QuestionComments.js
@@ -3,17 +3,47 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams, useHistory, Link } from "react-router-dom";
 
 const QuestionComments = ({questionId, user}) => {
+    const [showCommentBox, setShowCommentBox] = useState(false)
+    const [content, setContent] = useState('')
+    const [errors, setErrors] = useState([])
+    console.log("showCommentBox", showCommentBox)
+
+
 
     const addComment = () => {
         if (!user) {
             alert("You should be logged in first!")
         }
 
+
+        setShowCommentBox(!showCommentBox)
+
+
     }
+
+    const handleSubmit = async(e) => {
+
+    }
+
+
     return (
         <>
         <div className='question-comment-container'>
             <button className='question-comment-add' onClick={addComment}>Add a comment</button>
+        </div>
+        <div className='question-comment-box'>
+            {showCommentBox ?
+            <div>
+                <textarea className='question-comment-box-textarea'
+                    type='text'
+                    value={content}
+                    required
+                    onChange={(e) => setContent(e.target.value)}
+                />
+                <button className='question-comment-box-submit' onClick={handleSubmit}>Submit</button>
+            </div>
+            : null
+            }
         </div>
         </>
     )
