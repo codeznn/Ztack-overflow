1aacdd364a9bdab6fd22c99faf41967b92a091c2
Nannan Zhang <zhangnn8811@gmail.com>
2023-04-26 17:05:00 -0700

    user

--- react-app/src/components/Comments/QuestionComments.js
+++ react-app/src/components/Comments/QuestionComments.js
@@ -2,11 +2,20 @@ import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams, useHistory, Link } from "react-router-dom";
 
-const QuestionComments = ({questionId}, {user}) => {
+const QuestionComments = ({questionId, user}) => {
 
+    const addComment = () => {
+        if (!user) {
+            alert("You should be logged in first!")
+        }
 
+    }
     return (
-        <button>Add a comment</button>
+        <>
+        <div className='question-comment-container'>
+            <button className='question-comment-add' onClick={addComment}>Add a comment</button>
+        </div>
+        </>
     )
 };
 
